a0dc0449212f47aef58e814c10de3de83a805ce6
Danil-Belyaev <70798431+Danil-Belyaev@users.noreply.github.com>
2021-10-04 16:08:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[25];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-100, 100);
+                Console.Write(array[i] + " ");
+            }
+
+            int min = array[0];
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            if (min < 0)
+            {
+                Console.WriteLine("\nРазность наибольшего и наименьшего числа: " + (max + min));
+            }
+            else
+            {
+                Console.WriteLine("\nРазность наибольшего и наименьшего числа: " + (max - min));
+            }
+        }
+    }
+}
